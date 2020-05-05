6fa7773c81b80a572c9f21093ecf6ecfd71c4945
geronimogeek <geronimoid2017@gmail.com>
2020-05-05 16:34:23 +0700

    upload

---
+++ js/script.js
@@ -0,0 +1,14 @@
+// event pada saat diklik
+
+$('.page-scroll').on('click', function(e) {
+
+ var tujuan = $(this).attr('href');
+
+ var elemenTujuan = $(tujuan);
+
+ $('html , body').animate({
+  scrollTop: elemenTujuan.offset().top - 50
+ }, 1250);
+
+ e.preventDefault();
+});
